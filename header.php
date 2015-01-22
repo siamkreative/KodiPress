158500c5d4ac3190fa05925e550609e7a20056ae
Julien Vernet <julien@vernet.me>
2015-01-22 13:34:11 +0700

    Add Bootstrap container class

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site container">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'kodipress' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
