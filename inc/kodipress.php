febd941a7d3625b9cf81b6c49becf5284ffc3f38
Julien Vernet <julien@vernet.me>
2015-01-24 17:47:01 +0700

    Load jQuery in footer from Google CDN and remove momentjs

--- inc/kodipress.php
+++ inc/kodipress.php
@@ -6,12 +6,20 @@
 function kodipress_styles() {
 	wp_enqueue_style( 'kodipress-boostrap', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/css/bootstrap.min.css' );
 	wp_enqueue_style( 'kodipress-styles', get_stylesheet_directory_uri() . '/kodipress.css' );
+
+	/**
+	 * Load jQuery in footer from Google CDN
+	 */
+	if( !is_admin() ){
+		wp_deregister_script( 'jquery' );
+		wp_register_script( 'jquery', ( '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js' ), false, '2.1.3', true );
+		wp_enqueue_script( 'jquery' );
+	}
 	
 	wp_enqueue_script( 'kodipress-boostrap', '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js', array( 'jquery' ), '3.3.2', true );
 	wp_enqueue_script( 'kodipress-handlebars', '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js', array( 'jquery' ), '2.0.0', true );
 	wp_enqueue_script( 'kodipress-lazyload', '//cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js', array( 'jquery' ), '1.9.1', true );
 	wp_enqueue_script( 'kodipress-mixitup', '//cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js', array( 'jquery' ), '2.1.6', true );
-	wp_enqueue_script( 'kodipress-moment', '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js', array( 'jquery' ), '2.9.0', true );
 
 	wp_enqueue_script( 'kodipress-scripts', get_template_directory_uri() . '/js/kodipress.js', array( 'jquery' ), '1.0.0', true );
 }
