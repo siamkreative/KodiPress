a8b7fd09f7b52281fe8b7b2d2ca8125f8776b3a2
Julien Vernet <julien@vernet.me>
2015-01-24 17:44:58 +0700

    Remove unused stuff

--- functions.php
+++ functions.php
@@ -102,13 +102,7 @@ add_action( 'widgets_init', 'kodipress_widgets_init' );
 function kodipress_scripts() {
 	wp_enqueue_style( 'kodipress-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'kodipress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'kodipress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'kodipress_scripts' );
 
